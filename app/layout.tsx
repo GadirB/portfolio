63f5777b9a6dbf05c5fc9ec0e2473e0aa4381214
Qadir Babayev <babayev00qadir@gmail.com>
2025-02-21 17:13:45 +0400

    Home, StarBackground

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/main/StarBackground";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "This is my portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`@{inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        {children}
+      </body>
+    </html>
+  );
+}
